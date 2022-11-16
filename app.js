918319f5dd9af7c9748a6acd938ba43640c39244
Kian Zareinajad <kzareinajad@gmail.com>
2022-11-16 11:15:34 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+class Animal {
+    constructor(){
+        this.heath = 100;
+        this.hunger = 100;
+        this.thirsty = 100;
+        this.sleepiness = 100;
+        this.name ="No name";
+    }
+
+    feed(){
+        
+    }
+    walk(){
+
+    }
+    drink(){
+
+    }
+    sleep(){
+
+    }
+
+    animateSpite(){
+
+    }
+
+    updateHTML(){
+        
+    }
+
+}
+
