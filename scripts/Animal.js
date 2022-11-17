68b84233dcec3ff4e00f7273beb790580d27dbc2
Dave Hale <davejlhale@yahoo.co.uk>
2022-11-17 23:48:00 +0000

    working dog class extended from the base animal class

--- scripts/Animal.js
+++ scripts/Animal.js
@@ -4,7 +4,7 @@ export class Animal {
         //general pet info
         this.petName = name;
         this.petClass = this.constructor.name;
-
+        this.audio = new Audio();
         //these actions must be inherited methods from base class animal or be defined in an extended  class
         //html file must also have enough defined divs (for now) 1 per action in array
         this.petActions = ['feed', 'walk', 'drink', 'sleep', 'dance', "sing", "crawl"]
@@ -12,11 +12,13 @@ export class Animal {
         //when spawned this.alive will toggle to true also
         //see killPet() to toggle false
         this.alive = true;
+        //used to prevent an actions when false
+        this.active = true;
 
         //number is milliseconds per tick for reducing the stats in this.degradeStats() below
-        this.interval = 200;
-
-
+        this.interval = 10;
+        this.actionFeedback = "";
+        this.hurtSound = "../audio/Hurt.mp3"
         //current stats used are 
         //health hunger thirtsy sleepiness stamina happiness
 
@@ -30,11 +32,16 @@ export class Animal {
         this.happiness = 100;
 
         //how much damage the pet takes if any stat is 0
-        this.healthHit=1;
+        this.healthHit = .3;
+
+        //the number of stats and the % they need to all be above to heal
+        this.healPct =30;
+        this.healNumStats=5;
+        this.healRate =0.4;
 
         //max stats
-        this.maxHealth = 200;
-        this.maxHunger = 200;
+        this.maxHealth = 100;
+        this.maxHunger = 100;
         this.maxThirsty = 100;
         this.maxSleepiness = 100;
         this.maxStamina = 100;
@@ -49,103 +56,122 @@ export class Animal {
         this.boostHappiness = 100;
 
         //stat degradation
-        this.decreaseHunger = 1;
-        this.decreaseHealth = 1;
-        this.decreaseThirtsy = 1;
-        this.decreaseSleepiness = 1;
-        this.decreaseStamina = 1;
-        this.decreaseHappiness = 1;
+        this.decreaseHunger = 0.2;
+        this.decreaseThirtsy = 0.2;
+        this.decreaseSleepiness = .2;
+        this.decreaseStamina = .1;
+        this.decreaseHappiness = .25;
 
         //events - safeguard flag used in functions to prevent adding multiple listners
         this.evtsAdded = false;
+        this.hurtSound = new Audio(this.hurtSound);
+        this.deathMessage = "Your pet died!"
 
 
-       
-        
     }
+
+    //see dog class for example
+    //empty here for use akin to virtual function?
+    hoverEvents() { }
     //default actions feed walk drink sleep
     //pet action methods - these methods from base or extended by class must match this.petActions list 
     feed() {
+        if (!this.active) return
         console.debug(`${this.constructor.name} feed function called`);
         this.hunger += this.boostHunger;
     }
     walk() {
+        if (!this.active) return
         console.debug(`${this.constructor.name} walk function called`);
         this.stamina += this.boostStamina;
     }
     drink() {
+        if (!this.active) return
         console.debug(`${this.constructor.name} drink function called`);
         this.thirsty += this.boostThirtsy;
     }
     sleep() {
+        if (!this.active) return
         console.debug(`${this.constructor.name} sleep function called`);
         this.sleepiness += this.boostSleepiness;
     }
 
-    destroy(){
-        this.alive=false;
+    //helper function to simplify adding sound to any given pet action method
+    playSound(src) {
+        this.audio.currentTime = 0;
+        this.audio.src = src;
+        this.audio.play();
+    }
+
+    destroy() {
+        this.alive = false;
         console.log("destory");
     }
-    //reduce stats every interval by stated amounts defined by base or extended class
+    //reduce stats every interval by stated amounts defined 
+    //by base or extended class in constructor properties
     degradeStats() {
         let id = setTimeout(() => {
             clearTimeout(id);
             if (this.alive) {
-                //which stats degrade per interval
+                // stats degrade per interval
                 this.hunger = this.hunger -= this.decreaseHunger;
                 this.thirsty = this.thirsty -= this.decreaseThirtsy;
                 this.sleepiness = this.sleepiness -= this.decreaseSleepiness;
                 this.happiness = this.happiness -= this.decreaseHappiness;
                 this.stamina = this.stamina -= this.decreaseStamina;
-               let numZeroStats =  [this.hunger,this.thirsty,this.sleepiness,
-                     this.happiness,this.stamina].filter(x=>x<=0).length;
-                        this.health-=this.healthHit*numZeroStats;
-                     console.log(this.health,this.healthHit,numZeroStats)
-                {
-                    //do not remove
-                    this.updateHTML();
-                    this.reduceHealth();
-                    this.degradeStats();
-                }
+
+                //do not remove
+                this.checkHealth();
+                this.updateHTML();
+                this.degradeStats();
+
             }
         }, this.interval)
     }
 
-    //todo
-    //if any stats are 0 start to reduce pet health
-    reduceHealth() {
-        //for every stats type - stats not dynamically add to a pet so set by class properties
-        //currently 
-        //if stat==0
-        //reduce health by 1
-    }
-
     //DO NOT CHANGE BELOW
-
     // method to call when needs to be spawned
     spawnPet() {
         this.alive = true;
         this.assignActionButtons();
         this.degradeStats();
     }
-    killPet() {
-        this.health = 0;
-        this.alive = false;
+
+    //if any stats are 0 start to reduce pet health
+    checkHealth() {
+        //if any are 0 sound hurt
+        let numZeroStats = [this.hunger, this.thirsty, this.sleepiness,
+        this.happiness, this.stamina].filter(x => x <= 0).length;
+        if (numZeroStats > 0) {
+            this.health -= this.healthHit * numZeroStats;
+            if (this.hurtSound.paused)
+                this.hurtSound.play();
+        }
+        //if we are all above 
+        numZeroStats = [this.hunger, this.thirsty, this.sleepiness,
+        this.happiness, this.stamina].filter(x => x > this.healPct ).length;
+        if (numZeroStats == this.healNumStats) {
+            this.health += this.healRate;;
+            this.hurtSound.pause();
+        }
+        if (this.health <= 0) {
+            this.alive = false;
+            this.active = false;
+            this.actionFeedback = this.deathMessage;
+        }
     }
 
     //search html for all elements wiith class of petAction and rename them from list in this.petActions
     assignActionButtons() {
         if (this.evtsAdded) { return; }
         let elemBtns = document.getElementById('petActions');
-        elemBtns.innerHTML='';
+        elemBtns.innerHTML = '';
         console.log(elemBtns)
         this.petActions.forEach((action, index) => {
             let btnElem;
-
             btnElem = document.createElement("button");
             btnElem.innerHTML = action;
             btnElem.id = action;
-           
             elemBtns.appendChild(btnElem);
             btnElem.addEventListener('click', () => {
                 try {
@@ -153,16 +179,25 @@ export class Animal {
                 } catch (err) {
                     console.error(`looks like class or base class does not have the pet's action method defined`, err)
                 }
-            });
-           // this.addEvents();
-
-        })      
+            });//end evl
+        });//end of foreach
+        this.hoverEvents();
     }
 
     //update html - function called from this.degradeStats - its interval governs update tick/frame rate
     //finds all html element with a class="stats" and 
     //replaces each matching element's innerhtml with the class or exteneded class instances's matching stat
     updateHTML() {
+        let elem = document.getElementById('petActions');
+        if (!this.active || !this.alive) {
+            elem.style.backgroundColor = "#ee1111";
+            elem.style.color = "ee1111";
+            elem.style.opacity = "0.6";
+        } else {
+            elem.style.backgroundColor = "#fff";
+            elem.style.color = "#eb5e28";
+            elem.style.opacity = "1";
+        }
         this.clampStats();
         try {
             let petStats = document.getElementsByClassName('stats')
@@ -171,24 +206,28 @@ export class Animal {
             }
             Array.from(petStats).forEach(stat => {
                 stat.style.width = this[stat.id] + '%';
-                stat.innerHTML=  this[stat.id] + '%'       
+                stat.innerHTML = (this[stat.id]).toFixed(0) + '%'
             })
         } catch (err) {
             console.error(err);
         }
+
+        let feedback = document.getElementById('feedBackMsg');
+        feedback.innerHTML = this.actionFeedback;
     }
 
     clampStats() {
-        this.hunger = Math.min(Math.max(parseInt(this.hunger), 0), this.maxHunger);
-        this.thirsty = Math.min(Math.max(parseInt(this.thirsty), 0), this.maxThirsty);
-        this.sleepiness = Math.min(Math.max(parseInt(this.sleepiness), 0), this.maxSleepiness);
-        this.stamina = Math.min(Math.max(parseInt(this.stamina), 0), this.maxStamina);
-        this.health = Math.min(Math.max(parseInt(this.health), 0), this.maxHealth);
-        this.happiness = Math.min(Math.max(parseInt(this.happiness), 0), this.maxHappiness);
+        this.hunger = Math.min(Math.max((this.hunger), 0), this.maxHunger);
+        this.thirsty = Math.min(Math.max((this.thirsty), 0), this.maxThirsty);
+        this.sleepiness = Math.min(Math.max((this.sleepiness), 0), this.maxSleepiness);
+        this.stamina = Math.min(Math.max((this.stamina), 0), this.maxStamina);
+        this.health = Math.min(Math.max((this.health), 0), this.maxHealth);
+        this.happiness = Math.min(Math.max((this.happiness), 0), this.maxHappiness);
     }
 
     //todo
     animateSpite() { }
+
 }
 
 
--- scripts/dog.js
+++ scripts/dog.js
@@ -1,11 +1,115 @@
-import {Animal} from './Animal.js';
+import { Animal } from './Animal.js';
 
- export class Dog extends Animal{
-    constructor(name = "no name"){
+export class Dog extends Animal {
+    constructor(name = "no name") {
         super(name);
         console.debug("create dog");
-        this.petActions = ['feed',  'drink', 'sleep', 'walk','dance', "sing", "crawl"];
+        this.petActions = ['bark', 'feed', 'drink', 'sleep', 'walk', 'pet'];
+        this.hurtSound = "../audio/dogCrying.mp3"
+        this.hurtSound = new Audio(this.hurtSound);
+        this.interval = 30;
     }
 
+    //make all pet action methods start with
+    //if (!this.active) return
+    //this.audio.currentTime = 0;
+    //cant enter if pet is active on another we lock
+    //stop sound playing from previous actions
+    bark() {
+        if (!this.active) return
+        this.audio.currentTime = 0;
+        this.audio.src = ("../audio/bark.mp3");
+        this.audio.play();
+        this.thirsty -= 15;
+        this.stamina -= 15;
+        this.stamina += 25;
+    }
+
+    feed() {
+        if (!this.active) return
+        this.audio.currentTime = 0;
+        this.playSound("../audio/dogEating.mp3");
+        this.hunger += 35;
+        this.thirsty -= 5;
+    }
+    drink() {
+        if (!this.active) return
+        this.audio.currentTime = 0;
+        this.audio.src = "../audio/dogDrink.mp3";
+        this.audio.play();
+        this.thirsty += 25;
+        this.stamina -= 5;
+    }
+
+    //example of damage / improve over time
+    //using ugly but working setttimers
+    //
+    //NOTE the 
+    //this.active = false;
+    //which locks other methods untill the end of this toggles the flag
+
+    sleep(sleepCount = 5) {
+        if (!this.active) return
+        this.audio.currentTime = 0;
+        this.active = false;
+        this.playSound("../audio/dogSnoring.mp3");
+        setTimeout(() => { if (this.alive) { this.sleepiness += 10; this.happiness += 2; } }, 500);
+        setTimeout(() => { if (this.alive) { this.sleepiness += 10; this.happiness += 2; } }, 1000);
+        setTimeout(() => { if (this.alive) { this.sleepiness += 10; this.happiness += 2; } }, 1500);
+        setTimeout(() => { if (this.alive) { this.sleepiness += 10; this.happiness += 2; } }, 2000);
+        setTimeout(() => { if (this.alive) { this.sleepiness += 10; this.happiness += 2; } }, 2500);
+        setTimeout(() => { if (this.alive) { this.sleepiness += 10; this.happiness += 2; } }, 3000);
+        setTimeout(() => { if (this.alive) { this.sleepiness += 10; this.happiness += 2; } }, 3500);
+        setTimeout(() => { if (this.alive) { this.sleepiness += 10; this.happiness += 2; } }, 4000);
+        setTimeout(() => { if (this.alive) { this.sleepiness += 10; this.happiness += 2; } }, 4500);
+        setTimeout(() => { if (this.alive) { this.sleepiness += 10; this.active = true; } }, 5000, this.active);
+    }
+
+    walk() {
+        if (!this.active) return
+        this.active = false;
+        this.playSound("../audio/dogPanting.wav");
+        setTimeout(() => { if (this.alive) { this.sleepiness -= 3; this.stamina += 10; this.happiness += 20; } }, 500);
+        setTimeout(() => { if (this.alive) { this.sleepiness -= 3; this.stamina += 10; this.happiness += 20; } }, 1000);
+        setTimeout(() => { if (this.alive) { this.sleepiness -= 3; this.stamina += 10; this.happiness += 20; } }, 1500);
+        setTimeout(() => { if (this.alive) { this.sleepiness -= 3; this.active = true; } }, 2000);
+        this.stamina += 10;
+    }
+    //Action method not locked!! so can be spammed!
+    pet() {
+        this.happiness += 10;
+    }
+
+    //onhover events for action buttons - 
+    //this method called by base classes init sequence
+    //so we can override it within each extended class to override it
+    //id to search must be one from This classed constructor property array
+    //eg  this.petActions = ['bark', 'feed', 'drink', 'sleep', 'walk', 'pet'];
+    hoverEvents() {
+        let btn = document.getElementById('bark');
+        btn.addEventListener('mouseover', () => {
+            this.actionFeedback = "Improves Stamina by 25% but reduces thirst and hunger by 15%";
+        });
+        btn = document.getElementById('feed');
+        btn.addEventListener('mouseover', () => {
+            this.actionFeedback = "Improves Hunger by 35% but reduces thirst by 5%";
+        })
+        btn = document.getElementById('drink');
+        btn.addEventListener('mouseover', () => {
+            this.actionFeedback = "Improves Thirst by 25% but reduces stamina by 5%";
+        })
+        btn = document.getElementById('sleep');
+        btn.addEventListener('mouseover', () => {
+            this.actionFeedback = "Improves sleep by max 100 over 5 seconds but your pets asleep!!"
+        })
+        btn = document.getElementById('walk');
+        btn.addEventListener('mouseover', () => {
+            this.actionFeedback = "Improves sleep by max 100 but reduces stamina by 12 over 5 seconds, your pets out excercising"
+        })
+        btn = document.getElementById('pet');
+        btn.addEventListener('mouseover', () => {
+            this.actionFeedback = "Your pet loves being petted ANY time! +10%"
+        })
+    }
 }
 
